baee97bf43b54b54ece5418e44ebb345170eb30c
masp1234 <mart2c04@stud.kea.dk>
2022-02-19 18:40:31 +0100

    fix index.html

--- src/main/java/com/example/moviefacts/controller/MovieController.java
+++ src/main/java/com/example/moviefacts/controller/MovieController.java
@@ -1,10 +1,15 @@
 package com.example.moviefacts.controller;
 
 
+import com.example.moviefacts.model.Movie;
 import com.example.moviefacts.service.MovieService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
@@ -15,30 +20,40 @@ public class MovieController {
         this.movieService = new MovieService();
     }
 
+
+/*
     @GetMapping("/")
     public String index() {
         return "index";
     }
+*/
     @GetMapping("/getFirst")
     public String getFirst() {
         return movieService.getFirst().toString();
     }
+
     @GetMapping("/getRandom")
-    public String getRandom(){
+    public String getRandom() {
         return movieService.getRandom().toString();
     }
+    // Virker ikke
     @GetMapping("/getTenSortByPopularity")
-    public String getTenSortByPopularity(){
-        return movieService.getTenSortByPopularity().toString();
+    public String getTenSortByPopularity(Model model) {
+        List<Movie> movies = movieService.getTenSortByPopularity();
+        model.addAttribute("movies", movies);
+        return "get-ten-sort-by-popularity";
     }
+
     @GetMapping("/howManyWonAnAward")
-    public int howManyWonAnAward(){
+    public int howManyWonAnAward() {
         return movieService.howManyWonAnAward();
     }
+
     @GetMapping("getNAmountOfCharacters")
-    public String getNAmountOfCharacters(@RequestParam char character, int amount){
-        return movieService.getNAmountOfCharacters(character,amount).toString();
+    public String getNAmountOfCharacters(@RequestParam char character, int amount) {
+        return movieService.getNAmountOfCharacters(character, amount).toString();
     }
+
     @GetMapping("compareGenresByMovieLength")
     public String compareGenresByMovieLength(@RequestParam String genre1, String genre2) {
         return movieService.compareGenresByMovieLength(genre1, genre2);
