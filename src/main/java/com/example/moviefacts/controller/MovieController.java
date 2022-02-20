3b3122265333813fff4e12716074a0afc5f3a0d3
masp1234 <mart2c04@stud.kea.dk>
2022-02-20 13:21:43 +0100

    add html and css

--- src/main/java/com/example/moviefacts/controller/MovieController.java
+++ src/main/java/com/example/moviefacts/controller/MovieController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class MovieController {
     private MovieService movieService;
 
@@ -21,21 +21,27 @@ public class MovieController {
     }
 
 
-/*
+
     @GetMapping("/")
     public String index() {
         return "index";
     }
-*/
+
     @GetMapping("/getFirst")
-    public String getFirst() {
-        return movieService.getFirst().toString();
+    public String getFirst(Model model) {
+        Movie movie = movieService.getFirst();
+        model.addAttribute("movie", movie);
+        return "first-movie";
     }
 
+
     @GetMapping("/getRandom")
-    public String getRandom() {
-        return movieService.getRandom().toString();
+    public String getRandom(Model model) {
+        Movie movie = movieService.getRandom();
+        model.addAttribute("movie", movie);
+        return "random-movie";
     }
+
     // Virker ikke
     @GetMapping("/getTenSortByPopularity")
     public String getTenSortByPopularity(Model model) {
@@ -43,6 +49,7 @@ public class MovieController {
         model.addAttribute("movies", movies);
         return "get-ten-sort-by-popularity";
     }
+    /*
 
     @GetMapping("/howManyWonAnAward")
     public int howManyWonAnAward() {
@@ -58,5 +65,7 @@ public class MovieController {
     public String compareGenresByMovieLength(@RequestParam String genre1, String genre2) {
         return movieService.compareGenresByMovieLength(genre1, genre2);
     }
+
+ */
 }
 
